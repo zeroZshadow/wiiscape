bd6278f5f448668661aa60f2dff9a8dc147a0eac
Hamcha <zikyky@gmail.com>
2016-02-26 11:30:21 +0000

    Remove some unused functions

--- src/mathutils.c
+++ src/mathutils.c
@@ -2,77 +2,6 @@
 #include "mu.h"
 #include <ogc/gu.h>
 
-void EulerToQuaternion(guQuaternion* q, const f32 rX, const f32 rY, const f32 rZ) {
-	guVector vec;
-	vec.x = rX; vec.y = rY; vec.z = rZ;
-	muVecScale(&vec, &vec, 0.5f);
-
-	f32 x[2], y[2], z[2];
-
-	sincosf(vec.x, &x[0], &x[1]);
-	sincosf(vec.y, &y[0], &y[1]);
-	sincosf(vec.z, &z[0], &z[1]);
-
-	ps_eulerQuat(x, y, z, q);
-}
-
-void AxisAngleToQuaternion(guQuaternion* q, guVector rAxis, const f32 rAngle) {
-	f32 s, c;
-	sincosf(rAngle / 2.0f, &s, &c);
-	guVector out;
-	muVecScale(&rAxis, &out, s);
-	q->x = out.x;
-	q->y = out.y;
-	q->z = out.z;
-	q->w = c;
-}
-
-void QUAT_lookat(guVector* forward, guVector *up, guQuaternion* out) {
-	Mtx tmpMatrix;
-	guVector zero = { 0, 0, 0 };
-	guLookAt(tmpMatrix, forward, up, &zero);
-	c_guQuatMtx(out, tmpMatrix);
-}
-
-void QUAT_slerp(guQuaternion* q0, guQuaternion* q1, const float t, guQuaternion* out) {
-	if (t <= 0.0f) {
-		*out = *q0;
-		return;
-	} else if (t >= 1.0f) {
-		*out = *q1;
-		return;
-	}
-
-	f32 cosOmega;
-	QUAT_dotProduct(q0, q1, &cosOmega);
-	guQuaternion q0c = *q0, 
-				 q1c = *q1;
-	
-	if (cosOmega < 0.0f) {
-		f32 invert = -1.f;
-		QUAT_scale(&q1c, &q1c, &invert);
-		cosOmega = -cosOmega;
-	}
-
-	f32 k0, k1;
-	if (cosOmega > 0.9999f) {
-		k0 = 1.0f - t;
-		k1 = t;
-	} else {
-		f32 sinOmega = sqrt(1.0f - cosOmega*cosOmega);
-		f32 omega = atan2(sinOmega, cosOmega);
-		f32 oneOverSinOmega = 1.0f / sinOmega;
-
-		k0 = sin((1.0f - t) * omega) * oneOverSinOmega;
-		k1 = sin(t * omega) * oneOverSinOmega;
-	}
-	
-	QUAT_scale(&q0c, &q0c, &k0);
-	QUAT_scale(&q1c, &q1c, &k1);
-	guQuatAdd(&q0c, &q1c, out);
-	return;
-}
-
 /* This should be in gu.h or something */
 
 f32 guVecMag(guVector* vec) {
@@ -153,8 +82,6 @@ guVec2 guVec2MatMul(Mtx22 m, guVec2 v) {
 		m.b1 * v.x + m.b2 * v.y
 	};
 }
-
-
 
 guVec2 guVec2Abs(guVec2 a) {
 	return (guVec2) {
